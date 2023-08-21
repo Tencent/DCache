8234bbb5df126617813777753d9e29a67780b13e
BeyondWUXF <46481385+BeyondWUXF@users.noreply.github.com>
2023-08-21 11:21:13 +0800

    fix some error

--- web/publish.sh
+++ web/publish.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-TARS_K8S_WEB_HOST="http://127.0.0.1:3000"
+#TARS_K8S_WEB_HOST="http://127.0.0.1:3000"
 TARS_K8S_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJhZG1pbiIsImlhdCI6MTYzMjQ2MTg3NywiZXhwIjoxNzE4MDg0Mjc3fQ.2WqnAoNWS0qYXbSn_OuL18a59R4nkxrjwIeDjGii3QU"
-TARS_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJhZG1pbiIsImlhdCI6MTY0NjcyMTcxOCwiZXhwIjoxNzM5MjU2MTE4fQ.k6m3q4J3bR48vlkig3-T4QJvgJOwUyrlEyMS39xk554"
+#TARS_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJhZG1pbiIsImlhdCI6MTY0NjcyMTcxOCwiZXhwIjoxNzM5MjU2MTE4fQ.k6m3q4J3bR48vlkig3-T4QJvgJOwUyrlEyMS39xk554"
 TARS_K8S_BASE_IMAGE="tarscloud/tars.nodejsbase"
 
-APP=Base
+TARS_K8S_WEB_HOST=$1
+TARS_TOKEN=$2
 
-TARGET=TarsBenchmarkWeb
+APP=DCache
+
+TARGET=DCacheWebServer
 
 TARGET_PATH=tmp/${TARGET}
 
@@ -31,9 +34,9 @@ cd ${TARGET_PATH}
 echo "npm install --omit=dev"
 npm install --omit=dev
 
-mkdir tars_nodejs
+mkdir -p tars_nodejs
 
-cp -rf /Users/jarod/.nvm/versions/node/v16.13.0/lib/node_modules/@tars/node-agent tars_nodejs
+cp -rf node_modules/@tars/node-agent tars_nodejs
 
 cd ..
 
@@ -41,7 +44,7 @@ echo "tar czf ${TARGET}.tgz"
 tar czf ${TARGET}.tgz ${TARGET}
 
 # curl --progress-bar ${TARS_K8S_WEB_HOST}/pages/k8s/api/upload_and_publish?ticket=${TARS_K8S_TOKEN} -Fsuse=@${TARGET}.tgz -Fapplication=${APP} -Fmodule_name=${TARGET} -Fserver_type=nodejs  -Fbase_image=${TARS_K8S_BASE_IMAGE} -Fcomment=upload
-curl --progress-bar ${TARS_K8S_WEB_HOST}/pages/server/api/upload_and_publish?ticket=${TARS_TOKEN} -Fsuse=@${TARGET}.tgz -Fapplication=${APP} -Fmodule_name=${TARGET}  -Fcomment=upload
+curl --progress-bar ${TARS_K8S_WEB_HOST}/api/upload_and_publish?ticket=${TARS_TOKEN} -Fsuse=@${TARGET}.tgz -Fapplication=${APP} -Fmodule_name=${TARGET}  -Fcomment=upload
 
 echo ""
 
